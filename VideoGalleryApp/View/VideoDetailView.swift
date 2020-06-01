48aebaccbbf3e5f87ec13aeb79f7c8a379ab0303
mobile-apps-developer <apps.mobile.developer@gmail.com>
2020-06-01 15:45:50 +0530

    Play video in the AVPlayer

--- VideoGalleryApp/View/VideoDetailView.swift
+++ VideoGalleryApp/View/VideoDetailView.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
 struct VideoDetailView: View {
+
+    // MARK: - States
+
+    @State private var isVideoPlaying = false
+
     // MARK: - Properties
 
     let selectedVideo: Video
+    
 
     // MARK: - View
 
@@ -11,15 +17,23 @@ struct VideoDetailView: View {
         ScrollView {
             VStack {
                 VideoPreview(thumbnailURL: selectedVideo.thumbnail) {
+                    self.isVideoPlaying = true
                 }
                 VideoDescriptionView(
                     title: selectedVideo.name,
                     videoDescription: selectedVideo.videoDescription
                 )
             }
+            .padding(Constants.Metric.padding)
+            .sheet(isPresented: $isVideoPlaying) {
+                AVPlayerView(videoURL: URL(string: self.selectedVideo.videoLink)!)
+                    .edgesIgnoringSafeArea(.all)
+            }
         }
         .navigationBarItems(
-            trailing: Button(action: {}) {
+            trailing: Button(action: {
+
+            }) {
                 HStack {
                     Text(Constants.Texts.downloadVideo)
                     Image(systemName: Constants.SFSymbols.squardAndArrowDown)
