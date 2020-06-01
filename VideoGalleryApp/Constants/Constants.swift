8f111221486f023dd5c82e5a06ea9cded68285f3
mobile-apps-developer <apps.mobile.developer@gmail.com>
2020-06-01 15:42:15 +0530

    Create constant file for text and other constant values

---
+++ VideoGalleryApp/Constants/Constants.swift
@@ -0,0 +1,25 @@
+import CoreGraphics
+import Foundation
+import SwiftUI
+
+enum Constants {}
+
+// MARK: - Texts
+
+extension Constants {
+    enum Texts {
+        static let videos = "Videos"
+        static let downloadVideo = "Download Video"
+        static let cancelDownload = "Cancel Download"
+        static let loading = "Loading..."
+    }
+
+    // MARK: - SFSymbols
+}
+
+extension Constants {
+    enum SFSymbols {
+        static let playCircle = "play.circle"
+        static let squardAndArrowDown = "square.and.arrow.down"
+    }
+}
